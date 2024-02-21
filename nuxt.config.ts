a54b154605e5aedc025791fb34376707d7c780f7
Túlio Assis <tulioassis@Tulios-MacBook-Pro.local>
2024-02-21 09:49:09 -0300

    Initial implementatio of sass files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: [
+    // SCSS file in the project
+    "~/assets/style/main.scss", // you should add main.scss somewhere in your app
+  ],
 })
