90bfea550850169f81cc0bc8edeb5c2af1362024
Túlio Assis <tulioassis@Tulios-MacBook-Pro.local>
2024-02-28 20:15:30 -0300

    Add husk and config a git hook to run prettier

--- app.vue
+++ app.vue
@@ -1,18 +1,18 @@
 <template>
-  <main class="app">
-    <NuxtLayout>
-      <NuxtPage />
-    </NuxtLayout>
-  </main>
+	<main class="app">
+		<NuxtLayout>
+			<NuxtPage />
+		</NuxtLayout>
+	</main>
 </template>
 
 <style>
 body {
-    margin: 0;
-    height: 100%;
+	margin: 0;
+	height: 100%;
 }
 
 main {
-  height: 100vh;
+	height: 100vh;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,5 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>
-    Minha pagina inicial
-  </div>
+	<div>Minha pagina inicial</div>
 </template>
