776fefd7a2b03a061a267e99c5df6f2ca24e007f
Túlio Assis <tulioassis@Tulios-MacBook-Pro.local>
2024-03-07 14:13:06 -0300

    Add initial configuration for testing

--- .nuxt/dist/server/client.manifest.mjs
+++ .nuxt/dist/server/client.manifest.mjs
@@ -1,1 +1,1 @@
-export { default } from "file:///Users/tulioassis/Dev/frontend/leite-de-pedra/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs";
+export { default } from "file:///Users/tulioassis/Dev/frontend/leite-de-pedra/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
---
+++ tests/AppHeader.spec.ts
@@ -0,0 +1,11 @@
+import { describe, it, expect } from "vitest";
+import { mount } from "@vue/test-utils";
+
+import AppHeader from "@/components/AppHeader.vue";
+
+describe("AppHeader", () => {
+	it("is a Vue instance", () => {
+		const wrapper = mount(AppHeader);
+		expect(wrapper.vm).toBeTruthy();
+	});
+});
