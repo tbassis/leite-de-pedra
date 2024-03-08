09c991ccec8793d15de108b23707b92cf65c7cfb
Túlio Assis <tulioassis@Tulios-MacBook-Pro.local>
2024-03-08 11:33:00 -0300

    Fix identation of the page

--- pages/about.vue
+++ pages/about.vue
@@ -1,7 +1,5 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>
-    Sobre nós
-  </div>
+	<div>Sobre nós</div>
 </template>
--- pages/portfolio.vue
+++ pages/portfolio.vue
@@ -1,7 +1,5 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>
-    Portfolio
-  </div>
+	<div>Portfolio</div>
 </template>
